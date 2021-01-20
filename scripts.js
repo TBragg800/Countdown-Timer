a654ffa28beb871ba67ee0943c6295fd92f50027
LM&T <tbbragg83@gmail.com>
2021-01-20 13:26:47 -0600

    timer displays both the time left in countdown and the end time of countdown

---
+++ scripts.js
@@ -0,0 +1,37 @@
+let countDown;
+const timerDisplay = document.querySelector(".display__time-left");
+const endTime = document.querySelector(".display__end-time");
+
+function timer(seconds) {
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+    displayEndTime(then);
+    
+    countDown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        //check if we should
+        if (secondsLeft < 0) {
+            clearInterval(countDown);
+            return;
+        }
+        
+        //display it
+        displayTimeLeft(secondsLeft);
+    }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? "0" : ""}${remainderSeconds}`
+    document.title = display;
+    timerDisplay.textContent = display;
+}
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hours = end.getHours();
+    const mins = end.getMinutes();
+    endTime.textContent = `Be back at ${hours > 12 ? hours - 12 : hours}:${mins < 10 ? "0" : ""}${mins}`
+}
